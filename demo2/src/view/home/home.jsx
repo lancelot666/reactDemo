0cac24f12a4804b72c34c389ae8f21e8e9638a2e
Lancelot666 <249930369@qq.com>
2018-09-11 16:50:08 +0800

    111

--- demo2/src/view/home/home.jsx
+++ demo2/src/view/home/home.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {Link,Prompt} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as act from '../../redux/actions/homeAction';
 import { DatePicker } from 'antd';
-import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+//import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 //import { Switch, Route } from 'react-router-dom'
 //import User from '../user/user'
 class Home extends Component{
@@ -18,7 +18,8 @@ class Home extends Component{
 
     render(){
     	return(
-    		<div>
+    		<div> 	
+                <Prompt message="确定要离开？" />
                 <div>this is home</div>
                 <div><Link to='/orderList'>home to orderList</Link></div>
                 <DatePicker></DatePicker>
